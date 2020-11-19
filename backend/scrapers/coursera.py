99e8bdac63f2f1c82d53a27a2f69c79a8fc26a41
Gerald <gprmarquez@gmail.com>
2020-11-19 03:27:59 -0500

    tested coursera scraper to input data into a csv. Result successful

--- backend/scrapers/coursera.py
+++ backend/scrapers/coursera.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 baseUrl = "https://www.coursera.org"
 
@@ -21,8 +22,25 @@ page = requests.get(courseraUrl)
 # gets the html of the page
 soup = BeautifulSoup(page.text, 'html.parser')
 
-# beautifulsoup finds the text using a selector
-found = soup.find("h2", {'class': "color-primary-text"}).text
+# beautifulsoup finds the text for the title using a selector
+title = soup.find("h2", {'class': "color-primary-text"}).text
+# finds the text for all the titles on the page
+titles = soup.find_all("h2", {'class': "color-primary-text"})
+for title in titles:
+    print(title.text)
+
+dict_course = dict()
+
+with open('scraperData.csv', 'w+', newline='') as file:
+    fields = ['courseTitle']
+    writer = csv.DictWriter(file, fieldnames=fields)
+    writer.writeheader()
+
+    for i in range(len(titles)):
+        writer.writerow({'courseTitle': titles[i].text})
+
+
+# popularity = soup.find("span", {'class': "enrollment"}).text
 
 # output should be the text of the h2 selector
-print(found)
+# print(title)
