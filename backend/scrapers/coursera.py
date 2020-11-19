686ee9c54b270326b41bb45d72d047831283d05e
Gerald <gprmarquez@gmail.com>
2020-11-19 03:54:06 -0500

    coursera scraper now can scrape popularity and difficulty

--- backend/scrapers/coursera.py
+++ backend/scrapers/coursera.py
@@ -10,8 +10,8 @@ baseUrl = "https://www.coursera.org"
 
 # courseraUrl = "https://www.coursera.org/search?query=" + per20Skills
 
-# Testing an actual URL for scrapers. Uncomment above once the front-end
-# has an option to enter user input
+# *** Testing an actual URL for scrapers. Uncomment above once the front-end
+# has an option to enter user input ***
 
 # test Url
 courseraUrl = "https://www.coursera.org/search?query=scraping&"
@@ -24,23 +24,38 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 # beautifulsoup finds the text for the title using a selector
 title = soup.find("h2", {'class': "color-primary-text"}).text
+
 # finds the text for all the titles on the page
-titles = soup.find_all("h2", {'class': "color-primary-text"})
-for title in titles:
-    print(title.text)
+title_all = soup.find_all("h2", {'class': "color-primary-text"})
+# for title in title_all:
+#     print(title.text)
+
+popularity = soup.find("span", attrs={'class': "enrollment-number"}).text
+print(popularity)
+
+popularity_all = soup.find_all("span", attrs={'class': "enrollment-number"})
+# for popularity in popularity_all:
+#     print(popularity.text)
+
+difficulty = soup.find("span", attrs={'class': "difficulty"}).text
+print(difficulty)
+
+difficulty_all = soup.find_all("span", attrs={'class': "difficulty"})
+for difficulty in difficulty_all:
+    print(difficulty.text)
 
 dict_course = dict()
 
 with open('scraperData.csv', 'w+', newline='') as file:
-    fields = ['courseTitle']
+    fields = ['courseTitle', 'coursePopularity', 'courseDifficulty']
     writer = csv.DictWriter(file, fieldnames=fields)
     writer.writeheader()
 
-    for i in range(len(titles)):
-        writer.writerow({'courseTitle': titles[i].text})
-
-
-# popularity = soup.find("span", {'class': "enrollment"}).text
-
-# output should be the text of the h2 selector
-# print(title)
+    for i in range(len(title_all)):
+        writer.writerow(
+            {
+                'courseTitle': title_all[i].text,
+                'coursePopularity': popularity_all[i].text,
+                'courseDifficulty': difficulty_all[i].text
+            }
+        )
