f035e579c1416d742e42313a259e23d0ac9a43d1
pablomdd <pablo.mdd.007@gmail.com>
2020-11-24 23:37:11 -0600

    add form route

--- backend/api.py
+++ backend/api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -10,4 +10,13 @@ def api():
         'time': '20 hours',
         'difficulty': 'hard',
         'style': ['video', 'self-paced']
+    }
+
+@app.route('/api/form', methods=['POST'])
+def form():
+    content = request.get_json(silent=True)
+    print(content) # Do your processing
+
+    return {
+        'hey yo': "OK"
     }
